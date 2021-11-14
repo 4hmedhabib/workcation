2df04e97b255b0973be0639612c97abbdfbf026b
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-11-14 11:50:39 +0300

    added the site

---
+++ components/Layout.js
@@ -0,0 +1,5 @@
+const Layout = ({ children }) => {
+	return <div className="bg-gray-100 lg:grid grid-cols-2">{children}</div>;
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,47 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+import { Fragment } from 'react';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<Fragment>
+			<div className="flex flex-col items-center justify-center min-h-screen py-2">
+				<Head>
+					<title>Workcation || Book Your Escape</title>
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+				<div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:max-w-full">
+					<div className="xl:max-w-xl">
+						<img className="h-10" src="/img/logo.svg" alt="workcation logo" />
+						<img
+							className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover lg:hidden"
+							src="/img/beach-work.jpg"
+							alt="Woman workcation on the beach"
+						/>
+						<h1 className="mt-6 sm:mt-8 text-2xl sm:text-4xl font-bold text-gray-900">
+							You can work from anywhere. <br className="hidden lg:inline" />{' '}
+							<span className="text-indigo-500">Take advantage of it.</span>
+						</h1>
+						<p className="mt-2 text-gray-600 sm:mt-6 sm:text-xl sm:tracking-wider">
+							Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some
+							nice weather even when you're not on vacation.
+						</p>
+						<div className="mt-4">
+							<button className=" inline-block px-5 py-3 bg-indigo-600 text-white rounded-lg shadow-lg tracking-wider text-sm font-semibold uppercase">
+								Book Your Escape
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="hidden lg:block h-full bg-indigo-500">
+				<img
+					className="h-full object-cover object-center"
+					src="/img/beach-work.jpg"
+					alt="Woman workcation on the beach"
+				/>
+			</div>
+		</Fragment>
+	);
 }
